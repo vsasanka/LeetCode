3a4f2c79650e649d3994dc5e1eb38ff2cb8d35e7
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2021-07-13 05:40:05 +0000

    Sync LeetCode submission - Ugly Number (cpp)

---
+++ my-folder/problems/ugly_number/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        
+        if (n<=0){
+            return false;
+        }
+        
+        while(n!=1){
+            if (n%2==0){
+                n /= 2;
+                continue;
+            }
+            else if (n%3==0){
+                n /= 3;
+                continue;
+            }
+            else if (n%5 == 0){
+                n /= 5;
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
