dbacf7ca5c1eca7a89bef6b8cee981b6e12a608e
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2021-07-17 10:36:40 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- my-folder/problems/valid_anagram/solution.cpp
+++ my-folder/problems/valid_anagram/solution.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mappings;
-        map<char,int> mappingt;
-        map<char,int>::iterator itr;
-        int i;
-        char temp;
-        map<char,int>::iterator itr2;
         
-        for (i=0;i<s.size();i++){
-            itr = mappings.find(s[i]);
-            if (itr!=mappings.end()){
-                itr->second += 1;
-            }
-            else{
-                mappings.insert(pair<char,int> (s[i],1));
-            }
-        }
+        unordered_map<char,int> maps;
+        unordered_map<char,int> mapt;
         
-        for (i=0;i<t.size();i++){
-            itr = mappingt.find(t[i]);
-            if (itr!=mappingt.end()){
-                itr->second += 1;
-            }
-            else{
-                mappingt.insert(pair<char,int> (t[i],1));
-            }
-        }
-        
-        if (mappingt.size() != mappings.size()){
+        if (s.size() != t.size()){
             return false;
         }
         
-        for (itr2=mappingt.begin();itr2!=mappingt.end();itr2++){
-            itr = mappings.find(itr2->first);
-            if (itr==mappings.end() || (itr->second != itr2->second)){
+        for (int i=0; i<s.size(); i++){
+            if (maps.find(s[i])==maps.end()){
+                maps[s[i]] = 1;
+            }
+            else{
+                maps[s[i]]++;
+            }
+            
+            if (mapt.find(t[i])==mapt.end()){
+                mapt[t[i]] = 1;
+            }
+            else{
+                mapt[t[i]]++;
+            }
+        }
+        
+        if (mapt.size() != maps.size()){
+            return false;
+        }
+        
+        for (auto e : maps){
+            if (e.second != mapt[e.first]){
                 return false;
             }
         }
