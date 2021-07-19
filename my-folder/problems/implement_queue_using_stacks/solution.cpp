d70aea7ad03425625d78132cac53e7ea14e677ca
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2021-07-19 17:38:45 +0000

    Sync LeetCode submission - Implement Queue using Stacks (cpp)

---
+++ my-folder/problems/implement_queue_using_stacks/solution.cpp
@@ -0,0 +1,62 @@
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    stack<int> st1;
+    stack<int> st2;
+    
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (st2.empty()){
+            while (!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+
+        }
+        
+        int val = st2.top();
+        st2.pop();
+        return val;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        if (st2.empty()){
+            while (!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+
+        }
+        
+        return st2.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        if (st1.empty() && st2.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
