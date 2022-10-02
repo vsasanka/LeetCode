df8b420e5301b357e161adce9d475cb7df77b546
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-10-02 02:33:46 +0000

    Sync LeetCode submission - Number of Common Factors (cpp)

---
+++ my-folder/problems/number_of_common_factors/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        
+        int ans = 1;
+        
+        
+        
+        for (int i=2; i<=a, i<= b; i++){
+            if (a%i == 0 && b%i == 0){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
