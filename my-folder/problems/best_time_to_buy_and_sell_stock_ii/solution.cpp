e9df4aed3e9fc8e01f931b78d42d3a36d70eee21
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-10-12 00:07:56 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.cpp
@@ -1,53 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // pseudo code
-        /*
+        if (prices.size() < 2) return 0;
         
-        low_temp,high_temp = A[0];
+        int profit = 0;
         
-        for (iterate across array: i){
-            if (i+1==A.size()){
-            break;
-            }
-            if (A[i+1]>A[i]){
-                // low_temp = same;
-                high_temp = A[i+1];
-            }
-            else{
-                low_temp = A[i+1];
-                high_temp = A[i+1];
-                profit_total += high_temp - low_temp;
+        for (int i=0; i<prices.size()-1; i++){
+            if (prices[i+1] > prices[i]){
+                profit += prices[i+1] - prices[i];
             }
         }
-        */
         
-        int leng = prices.size();
-        int temp_low = prices[0];
-        int temp_high = prices[0];
-        int max_profit=0;
-        
-        if(leng==1){
-            return 0;
-        }
-        
-        // 1,2
-        
-        for (int i=0;i<leng;i++){
-            if( (i+1)==leng){
-                max_profit += temp_high - temp_low;
-            }
-            else if(prices[i+1]<prices[i]){
-                max_profit += temp_high - temp_low;
-                temp_high= prices[i+1];
-                temp_low = prices[i+1];
-            }
-            else if (prices[i+1]>=prices[i]){
-                temp_high=prices[i+1];
-            }
-            
-        }
-        
-        return max_profit;
+        return profit;
     }
 };
