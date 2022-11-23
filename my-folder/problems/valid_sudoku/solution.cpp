df4a6006876f15546160110535203064fa98c54f
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-11-23 00:26:50 +0000

    Sync LeetCode submission - Valid Sudoku (cpp)

--- my-folder/problems/valid_sudoku/solution.cpp
+++ my-folder/problems/valid_sudoku/solution.cpp
@@ -1,69 +1,81 @@
 class Solution {
-    
 private:
-    bool validateBox(vector<vector<char>>& board, int row, int col, set<int>& sett){
+    bool checkRow(vector<vector<char>>& board, int row){
+        vector<int> mp(9, 0);
         
-        for (int i=row;i<row+3;i++){
-            for (int j=col;j<col+3;j++){
-                if (board[i][j]=='.'){
-                    continue;
-                }
-                if (sett.find(board[i][j])!=sett.end()){
+        for (int i=0; i<9; i++){
+            char c = board[row][i];
+            
+            if (c != '.'){
+                mp[c - '1']++;
+                
+                if (mp[c - '1'] > 1){
                     return false;
-                }
-                else{
-                    sett.insert(board[i][j]);
                 }
             }
         }
-        sett.clear();
+        
         return true;
     }
+    
+    bool checkColumn(vector<vector<char>>& board, int col){
+        vector<int> mp(9, 0);
+        
+        for (int i=0; i<9; i++){
+            char c = board[i][col];
+            
+            if (c != '.'){
+                mp[c - '1']++;
+                
+                if (mp[c - '1'] > 1){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    bool checkBox(vector<vector<char>>& board, int row, int col){
+        vector<int> mp(9, 0);
+        
+        for (int i=row; i<row+3; i++){
+            for (int j=col; j<col+3; j++){
+                char c = board[i][j];
+            
+                if (c != '.'){
+                    mp[c - '1']++;
+
+                    if (mp[c - '1'] > 1){
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    
     
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        set<int> sett;
-        for (int i=0;i<9;i++){
-            for (int j=0;j<9;j++){
-                if (board[i][j]=='.'){
-                    continue;
-                }
-                if (sett.find(board[i][j])!=sett.end()){
-                    return false;
-                }
-                else{
-                    sett.insert(board[i][j]);
-                }
-            }
-            
-            sett.clear();
+        // validate row
+        
+        for (int i=0; i<9; i++){
+            if (!checkRow(board, i)) return false;
         }
         
-        for (int i=0;i<9;i++){
-            for (int j=0;j<9;j++){
-                if (board[j][i]=='.'){
-                    continue;
-                }
-                if (sett.find(board[j][i])!=sett.end()){
-                    return false;
-                }
-                else{
-                    sett.insert(board[j][i]);
-                }
-            }
-            
-            sett.clear();
+        // validate cols
+        for (int i=0; i<9; i++){
+            if (!checkColumn(board, i)) return false;
         }
         
-        bool ans=true;
-        
-        for (int i=0;i<9;i+=3){
-            for (int j=0;j<9;j+=3){
-                ans = ans && (validateBox(board, i,j, sett));
-                if (ans==false){
-                    return false;
-                }
+        // validate boxes
+        for (int i=0; i<9; i += 3){
+            for (int j=0; j<9; j += 3){
+                if (!checkBox(board, i, j)) return false;
             }
         }
         
