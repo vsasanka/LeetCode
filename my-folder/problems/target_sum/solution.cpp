117b4882d63d0e32ad67987ef69653054ca43c38
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-12-01 07:49:46 +0000

    Sync LeetCode submission - Target Sum (cpp)

--- my-folder/problems/target_sum/solution.cpp
+++ my-folder/problems/target_sum/solution.cpp
@@ -1,23 +1,36 @@
 class Solution {
-private:
-    void recursion(vector<int>& nums, int &target, int sum, int i, int &ans){
-        if (i >= nums.size()) {
-            if (sum == target){
-                ans++;
-            }
-            return ;
-        }
-        
-        recursion(nums, target, sum - nums[i], i+1, ans);
-        recursion(nums, target, sum + nums[i], i+1, ans);
-    }
     
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans = 0;
+        int sum = 0;
         
-        recursion(nums, target, 0, 0, ans);
+        sum = accumulate(nums.begin(), nums.end(), sum);
         
-        return ans;
+        vector<vector<int>> dp(2*sum+1, vector<int>(nums.size(), 0));
+        
+        dp[nums[0] + sum][0] = 1;
+        dp[(-1)*nums[0] + sum][0] += 1;
+        
+        for (int i=1; i<nums.size(); i++){
+            for (int j=0; j<2*sum+1; j++){
+                int a1 = 0;
+                int a2 = 0;
+                if (j-nums[i] >= 0) a1 = dp[j-nums[i]][i-1];
+                if (j+nums[i] < 2*sum+1) a2 = dp[j+nums[i]][i-1];
+                dp[j][i] = a1 + a2;
+            }
+        }
+        
+        // for (int i=0; i<nums.size(); i++){
+        //     for (int j=0; j<2*sum+1; j++){
+        //         cout << dp[j][i] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        if (target > sum || target + sum < 0) {
+            return 0;
+        }
+        return dp[target+sum][nums.size()-1];
     }
 };
