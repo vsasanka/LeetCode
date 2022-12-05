8455f507e071b5a39135634b1f0c1e82d44a554f
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-12-05 02:09:28 +0000

    Sync LeetCode submission - Combination Sum IV (cpp)

---
+++ my-folder/problems/combination_sum_iv/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1,0);
+
+        dp[0] = 1;
+        
+        for (int i=1; i<dp.size(); i++){
+            
+            //cout << i << endl;
+            
+            for (int j=0; j<nums.size(); j++){
+                if (i - nums[j] >= 0) {
+                    dp[i] = dp[i] + dp[ i - nums[j] ];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+};
