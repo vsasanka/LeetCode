ff0468a19090e6f478a1d5aa16d99ab491bf6e48
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-12-19 06:28:40 +0000

    Sync LeetCode submission - Sum of All Odd Length Subarrays (cpp)

--- my-folder/problems/sum_of_all_odd_length_subarrays/solution.cpp
+++ my-folder/problems/sum_of_all_odd_length_subarrays/solution.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        
-        vector<int> prefix(arr.size(), 0);
-        
-        prefix[0] = arr[0];
-        
-        for (int i=1; i<arr.size(); i++){
-            prefix[i] = prefix[i-1] + arr[i];
-        }
-        
-        int s = 0;
+        int n = arr.size();
+        int ans = 0;
         
         for (int i=0; i<arr.size(); i++){
-            for (int j=i; j<arr.size(); j += 2){
-                s += prefix[j] - prefix[i] + arr[i];
-            }
+            ans += arr[i]*(((i+1)*(n-i)+1)/2);
         }
         
-        return s;
+        return ans;
     }
 };
