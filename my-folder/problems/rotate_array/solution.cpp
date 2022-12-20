c016ca60ba7e0f4cdd146ddd50f147f11eb0373c
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2022-12-20 23:48:20 +0000

    Sync LeetCode submission - Rotate Array (cpp)

--- my-folder/problems/rotate_array/solution.cpp
+++ my-folder/problems/rotate_array/solution.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int len = nums.size();
-        k = k%len;
+        int n = nums.size();
+        k = k%n;
         
-        vector<int> nums_dup;
-        
-        for (int i=0;i<len;i++){
-            nums_dup.push_back(0);
-        }
-        
-        for (int i=0;i<len;i++){
-            nums_dup[(i+k)%len]=nums[i];
-            
-        }
-        
-        for (int i=0;i<len;i++){
-            nums[i]=nums_dup[i];
-        }
+        reverse(nums.begin(), nums.begin() + n-k);
+        reverse(nums.begin() + n - k, nums.begin() + n);
+        reverse(nums.begin(), nums.begin() + n);
     }
 };
