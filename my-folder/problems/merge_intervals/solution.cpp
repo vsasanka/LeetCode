b975d837f453e5db272220b5b2e61a3eb18d087b
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-01-24 07:29:20 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- my-folder/problems/merge_intervals/solution.cpp
+++ my-folder/problems/merge_intervals/solution.cpp
@@ -1,80 +1,37 @@
 class Solution {
 public:
+    static bool compare(const pair<int,int> &a, const pair<int,int> &b){
+        if (a.first == b.first) return a.second < b.second;
+        return a.first < b.first;
+    }
+
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-//         int left = intervals[0][0], right = intervals[0][1];
-//         vector<vector<int>> ans;
-        
-//         for (int i=0; i<intervals.size(); i++){
-//             left = min(intervals[i][0], left);
-//             right = max(intervals[i][1], right);
-            
-//             if (intervals[i][0] == intervals[i][1]){
-//                 vector<int> temp;
-//                 temp.push_back(intervals[i][0]);
-//                 temp.push_back(intervals[i][1]);
-//                 ans.push_back(temp);
-//             }
-//         }
-        
-//         vector<int> project(right - left + 1, 0);
-        
-//         for (int i=0; i<intervals.size(); i++){
-//             project[intervals[i][0] - left]++;
-//             project[intervals[i][1] - left]--;
-//         }
-        
-//         int check = 0;
-        
-//         int begin, end;
-//         int flag = 0;
-        
-//         for (int i=0; i<project.size(); i++){
-//             if (project[i] == 0){
-//                 continue;
-//             }
-            
-//             check += project[i];
-            
-//             if (check > 0 && flag == 0){
-//                 begin = i+left;
-//                 flag = 1;
-//             }
-//             else if (check == 0 && flag == 1){
-//                 end = i+left;
-//                 flag = 0;
-//                 vector<int> temp;
-//                 temp.push_back(begin);
-//                 temp.push_back(end);
-//                 ans.push_back(temp);
-//             }
-//         }
-        
-        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b){ return a[0] < b[0];});
-        
+        vector<pair<int,int>> ints;
         vector<vector<int>> ans;
-        int left = intervals[0][0], right = intervals[0][1];
-        
-        for (int i=1; i<intervals.size(); i++){
-            if (right < intervals[i][0]){
-                vector<int> temp;
-                
-                temp.push_back(left);
-                temp.push_back(right);
-                ans.push_back(temp);
-                
-                left = intervals[i][0];
-                right = intervals[i][1];
+
+        for (auto c: intervals){
+            ints.push_back({c[0],1});
+            ints.push_back({c[1]+1,-1});
+        }
+
+        sort(ints.begin(), ints.end(), compare);
+
+        int balance = 0;
+        int leftTemp = -1;
+
+        for (auto p: ints){
+            balance += p.second;
+
+            if (balance == 0){
+                ans.push_back({leftTemp, p.first-1});
+                leftTemp = -1;
             }
-            else{
-                right = max(intervals[i][1], right);
+
+            if (balance > 0 && leftTemp == -1){
+                leftTemp = p.first;
             }
         }
-        
-        vector<int> temp;
-        temp.push_back(left);
-        temp.push_back(right);
-        ans.push_back(temp);
-        
+
         return ans;
     }
 };
