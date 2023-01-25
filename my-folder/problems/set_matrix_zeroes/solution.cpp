8f91e93f451b152c993cc6362bfaf3e6f79d6218
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-01-25 04:15:17 +0000

    Sync LeetCode submission - Set Matrix Zeroes (cpp)

--- my-folder/problems/set_matrix_zeroes/solution.cpp
+++ my-folder/problems/set_matrix_zeroes/solution.cpp
@@ -1,72 +1,47 @@
 class Solution {
-    
-private:
-    void zerofyrow(vector<vector<int>>& matrix, int row){
-        
-        for (int i=0;i<matrix[0].size();i++){
-            matrix[row][i]=0;
-        }
-    }
-    
-private:
-    void zerofycol(vector<vector<int>>& matrix, int col){
-        for (int i=0;i<matrix.size();i++){
-            matrix[i][col]=0;
-        }
-    }
-    
 public:
+    void convertToZerosRow(vector<vector<int>>& matrix, int row){
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i=0; i<n; i++){
+            matrix[row][i] = 0;
+        }
+    }
+
+    void convertToZerosCol(vector<vector<int>>& matrix, int col){
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i=0; i<m; i++){
+            matrix[i][col] = 0;
+        }
+    }
+
     void setZeroes(vector<vector<int>>& matrix) {
         
-        int row_flag=0;
-        int col_flag = 0;
-        
-        for (int i=0;i<matrix[0].size();i++){
-            if (matrix[0][i]==0){
-                row_flag = 1;
-                break;
-            }
-        }
-        
-        for (int i=0;i<matrix.size();i++){
-            if (matrix[i][0]==0){
-                col_flag = 1;
-                break;
-            }
-        }
-        
-        for (int i=0;i<matrix.size();i++){
-            for (int j=0;j<matrix[0].size();j++){
-                if (matrix[i][j]==0){
-                    // cout << i << " " << j << endl;
-                    matrix[0][j]=0;
-                    matrix[i][0] = 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> rows;
+        vector<int> cols;
+
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (matrix[i][j] == 0){
+                    rows.push_back(i);
+                    cols.push_back(j);
                 }
             }
         }
-        
-        
-        
-        // cout << " ... " << endl;
-        for (int i=1;i<matrix[0].size();i++){
-            // cout << zeros[i].first << " " << zeros[i].second << endl;
-            if (matrix[0][i]==0){
-                zerofycol(matrix, i);
-            }
+
+        for (int i=0; i<rows.size(); i++){
+            convertToZerosRow(matrix, rows[i]);
         }
-        
-        for (int i=1;i<matrix.size();i++){
-            // cout << zeros[i].first << " " << zeros[i].second << endl;
-            if (matrix[i][0]==0){
-                zerofyrow(matrix, i);
-            }
+
+        for (int i=0; i<cols.size(); i++){
+            convertToZerosCol(matrix, cols[i]);
         }
-        
-        if (row_flag){
-            zerofyrow(matrix, 0);
-        }
-        if (col_flag){
-            zerofycol(matrix, 0);
-        }
+
+        return ;
     }
 };
