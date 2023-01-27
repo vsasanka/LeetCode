1ba412c30deef3c6e877fb0704b9d9a0775a257f
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-01-27 04:20:21 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

--- my-folder/problems/validate_binary_search_tree/solution.cpp
+++ my-folder/problems/validate_binary_search_tree/solution.cpp
@@ -10,55 +10,16 @@
  * };
  */
 class Solution {
-
-private:
-    bool recursion(TreeNode* root, int left_lim, int right_lim){
-        // check for range
-        
-        bool left_sub_tree = true;
-        bool right_sub_tree = true;
-        
-        if (root->val < left_lim || root->val > right_lim){
-            return false;
-        }
-        
-        // check if leaf node
-        
-        if (root->left==NULL && root->right == NULL){
-            return true;
-        }
-        
-        // check for left sub tree
-        
-        if (root->left!=NULL){
-            if (root->val==INT_MIN){
-                return false;
-            }
-            left_sub_tree = recursion(root->left, left_lim, root->val-1);
-        }
-        
-        // check for right sub tree
-        
-        if (root->right!=NULL){
-            if (root->val==INT_MAX){
-                return false;
-            }
-            right_sub_tree = recursion(root->right, root->val+1, right_lim);
-        }
-        
-        return left_sub_tree && right_sub_tree;
-    }
-    
 public:
+    bool recursion(TreeNode* node, long left, long right){
+        if (node == NULL) return true;
+
+        if (node->val <= left || node->val >= right) return false;
+
+        return recursion(node->left, left, node->val) && recursion(node->right, node->val, right);
+    }
+
     bool isValidBST(TreeNode* root) {
-        // int INT_MIN = ;
-        // int INT_MAX = ;
-        
-        if (root->left==NULL && root->right==NULL){
-            return true;
-        }
-        
-        
-        return recursion(root, INT_MIN, INT_MAX);
+        return recursion(root, LONG_MIN, LONG_MAX);
     }
 };
