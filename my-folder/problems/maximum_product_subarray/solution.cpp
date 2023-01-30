b5aaab8c674e44cd2a51099119014373bef0bd27
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-01-30 07:32:26 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- my-folder/problems/maximum_product_subarray/solution.cpp
+++ my-folder/problems/maximum_product_subarray/solution.cpp
@@ -2,34 +2,15 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         
-        int l = 0, r = 0;
-        int res = nums[0];
+        int l = 1, r = 1, ans = nums[0];
+        int n = nums.size();
         
         for (int i=0; i<nums.size(); i++){
-            l = (l?l:1) *nums[i];
-            r = (r ? r:1)*nums[nums.size() - 1 - i];
-            
-            res = max(res, max(l,r));
+            l = (l == 0) ? nums[i] : l*nums[i];
+            r = (r == 0) ? nums[n-i-1] : r*nums[n-i-1];
+            ans = max(ans, max(l,r));
         }
         
-        return res;
+        return ans;
     }
 };
-
-/*
-
-9 5 4 3 2
-
-9 5 4 0 2 3 4 
-
-9 -5 4 0 2 3 4 -1 -9
-
--9 -3 -4 0
-
--2
-
-2
-
-0
-
-*/
