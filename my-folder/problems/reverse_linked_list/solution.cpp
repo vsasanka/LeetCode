c6b537f4b8ac883dc15721720f36532f579315bc
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-01-30 19:41:43 +0000

    Sync LeetCode submission - Reverse Linked List (cpp)

--- my-folder/problems/reverse_linked_list/solution.cpp
+++ my-folder/problems/reverse_linked_list/solution.cpp
@@ -9,37 +9,27 @@
  * };
  */
 class Solution {
-
-private:
-    ListNode* reverseLink(ListNode* prev, ListNode* curr){
-        
-        ListNode* head_new;
-        if (curr->next==NULL){
-            curr->next = prev;
-            return curr;
-        }
-        else{
-            head_new = reverseLink(prev->next,curr->next);
-            curr->next = prev;
-            return head_new;
-        }
-    }
-    
 public:
     ListNode* reverseList(ListNode* head) {
-        
-        if (head==NULL){
-            return NULL;
+        ListNode* prev, *curr, *next;
+
+        if (head == NULL) return NULL;
+
+        if (head->next == NULL) return head;
+
+        prev = NULL;
+        curr = head;
+        next = head->next;
+
+        while (curr != NULL){
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            if (next == NULL)
+                return prev;
+            next = next->next;
         }
-        
-        if (head->next==NULL){
-            return head;
-        }
-        
-        ListNode* head_new = reverseLink(head, head->next);
-        
-        head->next = NULL;
-        
-        return head_new;
+
+        return curr;
     }
 };
