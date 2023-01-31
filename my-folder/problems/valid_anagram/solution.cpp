da8e808e98f8a6bdf19a8d59692a7c26c5676d1c
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-01-31 22:51:54 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- my-folder/problems/valid_anagram/solution.cpp
+++ my-folder/problems/valid_anagram/solution.cpp
@@ -1,40 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        unordered_map<char,int> maps;
-        unordered_map<char,int> mapt;
-        
-        if (s.size() != t.size()){
-            return false;
-        }
-        
-        for (int i=0; i<s.size(); i++){
-            if (maps.find(s[i])==maps.end()){
-                maps[s[i]] = 1;
-            }
-            else{
-                maps[s[i]]++;
-            }
-            
-            if (mapt.find(t[i])==mapt.end()){
-                mapt[t[i]] = 1;
-            }
-            else{
-                mapt[t[i]]++;
-            }
-        }
-        
-        if (mapt.size() != maps.size()){
-            return false;
-        }
-        
-        for (auto e : maps){
-            if (e.second != mapt[e.first]){
-                return false;
-            }
-        }
-        
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
