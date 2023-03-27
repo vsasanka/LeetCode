2c82ad71afbfbe0639cf3be711f1b1009e563de8
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-03-27 22:48:56 +0000

    Sync LeetCode submission - Minimum Path Sum (cpp)

--- my-folder/problems/minimum_path_sum/solution.cpp
+++ my-folder/problems/minimum_path_sum/solution.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+    void recursion(vector<vector<int>>& grid,vector<vector<int>> &dp, int sum, int row, int col){
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        
-        int sum = 0;
-        
-        for (int i=0; i<m; i++){
-            sum += grid[i][0];
-            
-            dp[i][0] = sum;
+
+        if (row == m || col == n || row < 0 || col < 0){
+            return ;
         }
-        
-        sum = 0;
-        
-        for (int i=0; i<n; i++){
-            sum += grid[0][i];
-            
-            dp[0][i] = sum;
-        }
-        
-        for (int i=1; i<m; i++){
-            for (int j=1; j<n; j++){
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        
+
+        if (sum >= dp[row][col]) return ;
+
+        dp[row][col] = min(dp[row][col], sum + grid[row][col]);
+
+        recursion(grid, dp, sum + grid[row][col], row+1, col);
+        recursion(grid, dp, sum + grid[row][col], row, col+1);
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        vector<vector<int>> dp (grid.size(), vector<int>(grid[0].size(), INT_MAX));
+
+        recursion(grid, dp, 0, 0, 0);
+
+        int m = grid.size();
+        int n = grid[0].size();
+
         return dp[m-1][n-1];
     }
 };
-
-
-/*
-
-1 4 5
-2 7 6
-6 8 7
-
-
-*/
