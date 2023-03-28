1e4963f3995f502d9ce61e6500a09dc89ff28b1f
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-03-28 22:51:03 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- my-folder/problems/group_anagrams/solution.cpp
+++ my-folder/problems/group_anagrams/solution.cpp
@@ -1,64 +1,38 @@
 class Solution {
 public:
+    string anaString(string input){
+        string ans = "";
+        
+        for (int i=0; i<26; i++){
+            ans += '0';
+        }
+
+        for (char c: input){
+            ans[c-'a'] = (ans[c-'a'] - '0' + 1) + '0';
+        }
+
+        return ans;
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        // create copy
-        
-        // sort each element of copy
-        
-        // compare each element with each other... similar ones found add them to a vector
-        // and push to answer. Take a bool array, mark true for all those which have been evaluted
-        // already
-        
-        vector<pair<string,int>> copy;
-        vector<string> group;
         vector<vector<string>> ans;
-        
-        for (int i=0;i<strs.size();i++){
-            copy.push_back(pair<string,int>(strs[i],i));
+        unordered_map<string, vector<string>> anagramMap;
+
+        for (int i=0; i<strs.size(); i++){
+            anagramMap[anaString(strs[i])].push_back(strs[i]);
         }
-        
-        vector<bool> flags(strs.size(), false);
-        
-        for (int i=0;i<strs.size();i++){
-            sort((copy[i].first).begin(),(copy[i].first).end());
+
+        for (auto v: anagramMap){
+            vector<string> anas;
+
+            for (string s: v.second){
+                anas.push_back(s);
+            }
+
+            ans.push_back(anas);
         }
-        
-        sort(copy.begin(),copy.end());
-        
-        /*
-        
-        for (int i=0;i<copy.size();i++){
-            if (flags[i]){
-                continue;
-            }
-            
-            group.push_back(strs[i]);
-            flags[i] = true;
-            
-            for (int j=i+1;j<copy.size();j++){
-                if (copy[i].compare(copy[j])==0){
-                    group.push_back(strs[j]);
-                    flags[j]=true;
-                }
-            }
-            ans.push_back(group);
-            group.clear();
-        }
-        */
-        
-        for (int i=0;i<strs.size();i++){
-            if ((i+1) < strs.size() && (copy[i].first).compare(copy[i+1].first)==0){
-                group.push_back(strs[copy[i].second]);
-            }
-            else{
-                group.push_back(strs[copy[i].second]);
-                ans.push_back(group);
-                group.clear();
-            }
-            
-        }
-        
+
         return ans;
     }
 };
+
