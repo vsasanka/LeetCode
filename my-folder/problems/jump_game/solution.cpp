6b229eba52b78e20f24d0d40754d560afa3ed3f4
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-03-29 08:12:28 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- my-folder/problems/jump_game/solution.cpp
+++ my-folder/problems/jump_game/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int jump = nums[0];
-        int n = nums.size();
+        int maxJump = 0;
+        int currPosition = 0;
 
-        for (int i=0; i<n; i++){
-            jump = max(nums[i] + i, jump);
+        for (int i=0; i<nums.size()-1; i++){
+            currPosition = i+1;
+            maxJump = max(nums[i]+currPosition, maxJump);
 
-            if (i == n - 1) return true;
-
-            if (jump <= i) return false;
+            if (currPosition >= maxJump) return false;
         }
 
         return true;
