0a1a0109db6ed5560d71e2fc1b86b7bdefaa6452
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-03-31 22:10:22 +0000

    Sync LeetCode submission - Number of Ways of Cutting a Pizza (cpp)

--- my-folder/problems/number_of_ways_of_cutting_a_pizza/solution.cpp
+++ my-folder/problems/number_of_ways_of_cutting_a_pizza/solution.cpp
@@ -1,44 +1,75 @@
 class Solution {
     
-private:
-    int dfs(int k, int r, int c, vector<vector<vector<int>>> &dp, vector<vector<int>> &preSum, int m, int n){
-        if (preSum[r][c] == 0) return 0;
-        
-        if (k == 0) return 1;
-        
-        int ans = 0;
-        
-        if (dp[k][r][c] != 0) return dp[k][r][c];
-        
-        for (int i=r; i<m; i++){
-            if (preSum[r][c] - preSum[i][c] > 0){
-                ans = (ans + dfs(k-1, i,c,dp, preSum,m,n))%((int)pow(10,9) + 7);
-            }
-        }
-        
-        for (int i=c; i<n; i++){
-            if (preSum[r][c] - preSum[r][i] > 0){
-                ans = (ans + dfs(k-1, r,i,dp, preSum,m,n))%((int)pow(10,9) + 7);
-            }
-        }
-        
-        return dp[k][r][c] = ans;
-    }
-    
 public:
+    void applesCount(vector<vector<int>> &apples, vector<string>& pizza){
+        int m = pizza.size();
+        int n = pizza[0].size();
+
+        if (pizza[m-1][n-1] == 'A') apples[m-1][n-1] = 1;
+        
+        for (int i=m-2; i>=0; i--){
+            if (pizza[i][n-1] == 'A') apples[i][n-1] = 1 + apples[i+1][n-1];
+            else apples[i][n-1] = apples[i+1][n-1];
+        }
+
+        for (int i=n-2; i>=0; i--){
+            if (pizza[m-1][i] == 'A') apples[m-1][i] = 1 + apples[m-1][i+1];
+            else apples[m-1][i] = apples[m-1][i+1];
+        }
+
+        for (int i=m-2; i>=0; i--){
+            for (int j=n-2; j>=0; j--){
+                if (pizza[i][j] == 'A') apples[i][j] = 1 + apples[i+1][j] + apples[i][j+1] - apples[i+1][j+1];
+                else apples[i][j] = apples[i+1][j] + apples[i][j+1] - apples[i+1][j+1];
+            }
+        }
+    }
+
+    int recursion(vector<vector<int>> &apples, int k, int row, int col, vector<vector<vector<int>>> &dp){
+        int m = apples.size();
+        int n = apples[0].size();
+        int MOD = 1000000007;
+
+        if (row == m || col == n) return 0;
+        if (apples[row][col] == 0) return 0;
+        if (k == 0) return 1;
+        if (dp[row][col][k] != -1) return dp[row][col][k];
+
+        dp[row][col][k] = 0;
+
+        for (int i=row+1; i<m; i++){
+            if (apples[row][col] - apples[i][col] > 0) dp[row][col][k] = (dp[row][col][k] + recursion(apples, k-1, i, col, dp))%MOD;
+        }
+
+        for (int i=col+1; i<n; i++){
+            if (apples[row][col] - apples[row][i] > 0) dp[row][col][k] = (dp[row][col][k] + recursion(apples, k-1, row, i, dp))%MOD;
+        }
+        return dp[row][col][k];
+    }
+
     int ways(vector<string>& pizza, int k) {
         int m = pizza.size();
         int n = pizza[0].size();
         
-        vector<vector<vector<int>>> dp(k, vector<vector<int>>(m, vector<int>(n, 0)));
-        vector<vector<int>> preSum(m+1, vector<int>(n+1, 0));
-        
-        for (int i=m-1; i>=0; i--){
-            for (int j=n-1; j>=0; j--){
-                preSum[i][j] = preSum[i+1][j] + preSum[i][j+1] - preSum[i+1][j+1] + (pizza[i][j] == 'A' ? 1:0);
+        vector<vector<int>> apples(m, vector<int>(n,0));
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k, -1)));
+
+        applesCount(apples, pizza);
+
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                cout << apples[i][j] << " ";
             }
+            cout << endl;
         }
         
-        return dfs(k-1,0,0,dp,preSum, m, n);
+        return recursion(apples, k-1, 0, 0, dp);
     }
 };
+
+
+/*
+
+
+
+*/
