f3796177ba333803e99ed41921e21edf7bee2390
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-04-06 21:25:46 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

--- my-folder/problems/maximum_depth_of_binary_tree/solution.cpp
+++ my-folder/problems/maximum_depth_of_binary_tree/solution.cpp
@@ -10,37 +10,19 @@
  * };
  */
 class Solution {
-    
-private:
-    int recursion(TreeNode* root, int max_val){
-        int left,right;
-        max_val +=1;
-        left = max_val;
-        right = max_val;
-        
-        if (root->left==NULL && root->right==NULL){
-            return max_val;
-        }
-        
-        if (root->left!=NULL){
-            left = recursion(root->left, max_val);
-        }
-        
-        if (root->right!=NULL){
-            right = recursion(root->right, max_val);
-        }
-        
-        return max(left, right);
-    }
-    
 public:
+    int maxDepth(TreeNode* root, int depth){
+        if (root == NULL) return depth;
+
+        depth++;
+
+        return max(maxDepth(root->left, depth), maxDepth(root->right, depth));
+        
+    }
+
     int maxDepth(TreeNode* root) {
-        int max_val = 0;
-        
-        if (root==NULL){
-            return 0;
-        }
-        
-        return recursion(root, max_val);
+        int depth = 0;
+
+        return maxDepth(root, depth);
     }
 };
