3f076660324edaf685730fdef117997eb8fa3309
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2023-04-10 04:56:34 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

--- my-folder/problems/longest_increasing_subsequence/solution.cpp
+++ my-folder/problems/longest_increasing_subsequence/solution.cpp
@@ -7,8 +7,8 @@ public:
         
         for (int i=0; i<nums.size(); i++){
             for (int j=0; j<i; j++){
-                if (nums[j] < nums[i] && dp[i] < dp[j] + 1){
-                    dp[i] = dp[j] + 1;
+                if (nums[j] < nums[i]){
+                    dp[i] = max(dp[j] + 1, dp[i]);
                 }
             }
         }
